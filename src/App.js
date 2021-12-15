ef51b05fd12903b8c655e60eb89923f03f0a30ef
Nishant Nimbalkar <nishant.nimbalkar@sap.com>
2021-12-15 18:31:59 +0530

    Added Content Tab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Sources } from "./components/Sources";
 import Navbar from "./components/Navbar";
+import { Content } from "./components/Content";
 
 class App extends React.Component {
   constructor() {
@@ -24,6 +25,12 @@ class App extends React.Component {
           <div className="row">
             <div className="col-sm-4">
               <Sources updateSource={this.updateSource.bind(this)} />
+            </div>
+            <div className="col-sm-8 fixed-content">
+              <Content
+                source={this.state.source}
+                sourceName={this.state.sourceName}
+              />
             </div>
           </div>
         </div>
